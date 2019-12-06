c2186f2ca6a6aad0d6cceac57feb81f8ad9fb37e
wangqi <wangqi@localhost.localdomain>
2019-12-06 03:12:30 +0000

    验证微信接口

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -7,5 +7,22 @@ use Illuminate\Http\Request;
 
 class WxController extends Controller
 {
-    //
+     public function wechet(){
+        $tojen = '2259b56f5898cd6192c50';
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+	
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+    
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+     }
 }
