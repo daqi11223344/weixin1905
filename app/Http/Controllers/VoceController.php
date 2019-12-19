c08b1efd8f78dc9733bd01202c56251f58f8de78
wangqi <wangqi@localhost.localdomain>
2019-12-19 14:34:55 +0800

    获取头像

--- app/Http/Controllers/VoceController.php
+++ app/Http/Controllers/VoceController.php
@@ -41,7 +41,7 @@ class VoceController extends Controller
             Redis::Zadd($key,time(),$openid);
         }
         $total = Redis::zCard($key);        // 获取总数
-
+        echo '投票总人数： '.$total;echo '</br>';
         $members = Redis::zRange($key,0,-1,true);       // 获取所有投票人的openid
 
         foreach($members as $k=>$v){
