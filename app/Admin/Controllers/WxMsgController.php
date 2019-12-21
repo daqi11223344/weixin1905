7ef6a81718914fb65d88d3da2a197fb884e6dad5
wangqi <wangqi@localhost.localdomain>
2019-12-21 13:40:09 +0800

    access_token

--- app/Admin/Controllers/WxMsgController.php
+++ app/Admin/Controllers/WxMsgController.php
@@ -21,13 +21,17 @@ class WxMsgController extends AdminController
     public function sendMsg(){
         echo __METHOD__;
 
+
+        $weather_api='https://free-api.heweather.net/s6/weather/now?location=beijing&key=090802638cdf46d4bd6162c2940cc871';
+        $weather_info = file_get_contents($weather_api);
+
         $openid = WxUserModel::select('openid','nickname','sex')->get()->toArray();
 //        echo '<pre>';print_r($openid);echo '</pre>';
 
         $open = array_column($openid,'openid');
         echo '<pre>';print_r($open);echo '</pre>';
 
-        $url = 'https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token=28_ekgqIwW5K0seSKb2Udr8Mh_o-EVm91oi3MeDg5CIh9uZ_yolwiVpfD7SSF2FU-W0v4Cdanpaw7tk5ddsuAiYBEAU91RofVGzHTHsbkT3at5nd6guaJx24Ut3h3-Zyyrg4GWGjQsxcqrC3lFKNKJdAAAWTS';
+        $url = 'https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token=28_vC6K8sjCp4hB75B65eohuAVXll7IaSmgEgdpYgorXSHWuJRgEqrUrZ2K08Hco7-qTGAv9beCgdeaN5vsXuwxgU63q1_sNrR0qQmA3SP7W5KTYtSqsBtZPs_Rpf3yvh-wsevophoixdXU2Ue-ZMXeAEAUHS';
         $msg = date('Y-m-d H:i:s') ."\n" . '那个 This is my nake 的微信测试号，you look look and try 一下 ';
 
         $data = [
@@ -41,7 +45,7 @@ class WxMsgController extends AdminController
             'body'  => json_encode($data,JSON_UNESCAPED_UNICODE)
         ]);
 
-        echo $response->getBody();
+        echo $response->getBody();echo "\n";
 
 
     }
