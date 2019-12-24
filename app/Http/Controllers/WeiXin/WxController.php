2b391e1bc35127ecf866af2838c474815108de5d
wangqi <wangqi@localhost.localdomain>
2019-12-24 11:33:23 +0800

    分享

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -499,6 +499,8 @@ class WxController extends Controller
         return redirect($url);
     }
 
+    
+
 
 
 }
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -191,8 +191,8 @@
         wx.updateAppMessageShareData({
             title: '分享测试', // 分享标题
             desc: '描述', // 分享描述
-            link: 'http://wangqi.bianaoao.top/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://wangqi.bianaoao.top/img/2ha.jpg', // 分享图标
+            link: 'http://wangqi.bianaoao.top/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://wangqi.bianaoao.top/wx_media/imgs/2019121414155029246.jpeg', // 分享图标
             success: function () {
                 // 设置成功
                 alert(11111);
@@ -203,8 +203,8 @@
         wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
             wx.updateTimelineShareData({
                 title: '分享测试', // 分享标题
-                link: 'http://wangqi.bianaoao.top/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                imgUrl: 'http://wangqi.bianaoao.top/img/2ha.jpg', // 分享图标
+                link: 'http://wangqi.bianaoao.top/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://wangqi.bianaoao.top/wx_media/imgs/2019121414155029246.jpeg', // 分享图标
                 success: function () {
                     alert("分享成功");
                 }
